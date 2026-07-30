caa1d05600e62b4d71d4f49d1a8d38038575d87f
Tracymboya-coder <trcakoth@gmail.com>
2026-07-30 17:07:08 +0300

    applayout change

--- frontend/src/components/AppLayout.jsx
+++ frontend/src/components/AppLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 
-export default function AppLayout() {
+export default function Applayout() {
   return (
     <div className="min-h-screen bg-basket-cream">
       <Navbar />
